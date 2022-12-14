5f3fb7293fd6d2080950cc5588e3d147ef2b3205
Chirag Saraswat <csaraswat87@gmail.com>
2022-12-14 19:29:18 +0530

    add user form added

--- docker_node/src/index.ts
+++ docker_node/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./db";
 import { router as userRoutes } from "./routes/userRoutes";
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 3000;
--- docker_react/src/App.tsx
+++ docker_react/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import { AxiosError } from "axios";
+import { toast } from "react-toastify";
 type Props = {};
 
 function App({}: Props) {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
+  const [result, setResult] = useState(null);
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
@@ -13,7 +15,21 @@ function App({}: Props) {
     setName(e.target.value);
   };
 
-  const handleFormSubmit = 
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("http://localhost:5000/users", {
+        name,
+        email,
+      });
+      setResult(data);
+      toast.success("user added");
+    } catch (error) {
+      const err = error as AxiosError;
+      setResult(null);
+      toast.error(String(err.response?.data));
+    }
+  };
 
   return (
     <div className="container mx-auto">
@@ -22,23 +38,34 @@ function App({}: Props) {
       </h1>
       <div>
         <h3 className="text-2xl text-center mt-8">Add User</h3>
-        <form className="flex mt-6 items-center  flex-col space-y-4">
+        <form
+          className="flex mt-6 items-center  flex-col space-y-4"
+          onSubmit={handleFormSubmit}
+        >
           <input
             className="border border-slate-400 p-2 rounded-md"
             placeholder="Enter Name"
             value={name}
             onChange={handleNameChange}
+            type="text"
           />
           <input
             placeholder="Enter Email"
             value={email}
             className="rounded-md border border-slate-400 p-2"
             onChange={handleEmailChange}
+            type="email"
           />
-          <button className="bg-purple-600 p-2 text-white rounded-lg">
+          <button
+            type="submit"
+            className="bg-purple-600 p-2 text-white rounded-lg"
+          >
             Add User
           </button>
         </form>
+      </div>
+      <div className="flex justify-center mt-6">
+        <pre>{result && JSON.stringify(result, null, 2)}</pre>
       </div>
     </div>
   );
