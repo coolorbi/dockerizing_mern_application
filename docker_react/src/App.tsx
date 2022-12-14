c59c01d7dc5cf839aeab70fe22ef3d1bd9a7a830
Chirag Saraswat <csaraswat87@gmail.com>
2022-12-14 18:51:56 +0530

    adding tailwind css

--- docker_react/src/App.tsx
+++ docker_react/src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+type Props = {};
+
+function App({}: Props) {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleFormSubmit = 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <h1 className="text-3xl text-center mt-6">
+        Dockerizing Mern Application Using Docker and Docker Compose
+      </h1>
+      <div>
+        <h3 className="text-2xl text-center mt-8">Add User</h3>
+        <form className="flex mt-6 items-center  flex-col space-y-4">
+          <input
+            className="border border-slate-400 p-2 rounded-md"
+            placeholder="Enter Name"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <input
+            placeholder="Enter Email"
+            value={email}
+            className="rounded-md border border-slate-400 p-2"
+            onChange={handleEmailChange}
+          />
+          <button className="bg-purple-600 p-2 text-white rounded-lg">
+            Add User
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
